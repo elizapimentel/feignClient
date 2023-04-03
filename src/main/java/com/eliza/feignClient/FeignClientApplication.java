5a6acb719a78ae4ecb8460f5c7d679345e264a8b
eliza-pimentel <eliza.pimentel@ibm.com>
2023-04-03 13:20:34 -0300

    :tada: first commit

---
+++ src/main/java/com/eliza/feignClient/FeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.eliza.feignClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientApplication.class, args);
+	}
+
+}
