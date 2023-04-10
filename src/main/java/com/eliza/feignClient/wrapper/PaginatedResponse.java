19245aa75492a3eeb099b7f037eca2a50d3c704c
eliza-pimentel <eliza.pimentel@ibm.com>
2023-04-10 14:00:43 -0300

    :sparkles: feat: add wrapper class to implement page test

---
+++ src/main/java/com/eliza/feignClient/wrapper/PaginatedResponse.java
@@ -0,0 +1,43 @@
+package com.eliza.feignClient.wrapper;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PaginatedResponse<T> extends PageImpl<T> {
+
+    private boolean first;
+    private boolean last;
+    private int totalPages;
+    private int numberOfElements;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES )
+    public PaginatedResponse(@JsonProperty("content") List<T> content,
+                             @JsonProperty("number") int number,
+                             @JsonProperty("size") int size,
+                             @JsonProperty("totalElements") int totalElements,
+                             @JsonProperty("last") boolean last,
+                             @JsonProperty("first") boolean first,
+                             @JsonProperty("totalPages") int totalPages,
+                             @JsonProperty("numberOfElements") int numberOfElements,
+                             @JsonProperty("pageable") JsonNode pageable,
+                             @JsonProperty("sort") JsonNode sort) {
+        super(content, PageRequest.of(number, size), totalElements);
+
+        this.last = last;
+        this.first = first;
+        this.totalPages = totalPages;
+        this.numberOfElements = numberOfElements;
+
+    }
+}
